25f408c2b6fc550783fca5c2cd5e96894a130ca5
***** <kn47ytv9mv@privaterelay.appleid.com>
2026-08-18 09:11:24 -0400

    Added Jenkins.

--- resources.tf
+++ resources.tf
@@ -2,6 +2,16 @@ module "aws_vpc" {
   source = "git@github.com:kn47ytv9mv/aws-vpc"
 }
 
+module "aws_jenkins" {
+  source = "git@github.com:kn47ytv9mv/aws-jenkins"
+
+  private_subnet_ids = module.aws_vpc.private_subnet_ids
+  public_subnet_ids  = module.aws_vpc.public_subnet_ids
+  domain             = module.aws_vpc.domain_name
+  route53_zone_id    = module.aws_vpc.route53_zone_id
+  vpc_id             = module.aws_vpc.vpc_id
+}
+
 import {
   id = var.backend
   to = aws_s3_bucket.backend
